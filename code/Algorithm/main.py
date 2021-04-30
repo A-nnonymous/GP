c6b6f78ba277d0c3d4d9597ea6aa79d7b020c9cf
Zhaowu.P <panzhaowu7106@link.tyut.edu.cn>
2021-04-30 15:17:28 +0800

    First stable edition of training frameworks.

--- code/Algorithm/main.py
+++ code/Algorithm/main.py
@@ -1,22 +1,24 @@
-from PyTsetlinMachineCUDA.tm import RegressionTsetlinMachine
-from PyTsetlinMachineCUDA.tools import Booleanizer
+from pyTsetlinMachineParallel.tm import RegressionTsetlinMachine
+from pyTsetlinMachineParallel.tools import Binarizer
 import numpy as np
 from time import time
 from sklearn.model_selection import train_test_split
 import pandas as pd
+import numpy as np
 
-f1=open("ConfirmedTimeSeriesFeature.csv","r")
+f1=open("TFeatures.csv","r")
 data=pd.read_csv(f1).to_numpy()
-f2=open("ConfirmedTimeSeriesOUT.csv","r")
-target=pd.read_csv(f2)["Washington"].to_numpy()
-X = data
-Y = target
+f2=open("TTarget.csv","r")
+target=pd.read_csv(f2).to_numpy().flatten()
 
-b = Booleanizer(max_bits_per_feature = 20)
+X = data
+Y =target
+
+b = Binarizer(max_bits_per_feature = 16)
 b.fit(X)
 X_transformed = b.transform(X)
 
-tm = RegressionTsetlinMachine(10000, 500*10, 2.75)
+tm = RegressionTsetlinMachine(80000, 20000,32,weighted_clauses=True)
 
 print("\nRMSD over 25 runs:\n")
 tm_results = np.empty(0)
@@ -24,8 +26,9 @@ for i in range(25):
 	X_train, X_test, Y_train, Y_test = train_test_split(X_transformed, Y)
 
 	start = time()
-	tm.fit(X_train, Y_train, epochs=100)
+	tm.fit(X_train, Y_train, epochs=20)
 	stop = time()
-	tm_results = np.append(tm_results, np.sqrt(((tm.predict(X_test) - Y_test)**2).mean()))
 
+	tm_results = np.append(tm_results, np.sqrt(((tm.predict(X_test) - Y_test)**2).mean()))
+	out=pd.DataFrame(tm.predict(X)).to_csv("output.csv")
 	print("#%d RMSD: %.2f +/- %.2f (%.2fs)" % (i+1, tm_results.mean(), 1.96*tm_results.std()/np.sqrt(i+1), stop-start))
